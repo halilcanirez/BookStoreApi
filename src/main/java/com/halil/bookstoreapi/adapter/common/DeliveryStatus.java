9b6e5ec712f82a2a2e9f6ed5897a2cd3efeb1b6b
Halil Can İrez <halil9819@gmail.com>
2022-04-13 14:37:20 +0300

    common

---
+++ src/main/java/com/halil/bookstoreapi/adapter/common/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.halil.bookstoreapi.adapter.common;
+
+public enum DeliveryStatus {
+    RECEIVED, SHIPPED,DISTRIBUTION, DELİVERED
+}
---
+++ src/main/java/com/halil/bookstoreapi/adapter/jpa/common/Category.java
@@ -0,0 +1,14 @@
+package com.halil.bookstoreapi.adapter.jpa.common;
+
+
+public enum Category {
+        LITERATURE,
+        NONFICTION,
+        ACTION,
+        THRILLER,
+        TECHNOLOGY,
+        DRAMA,
+        POETRY,
+        MEDIA,
+        OTHERS
+}
